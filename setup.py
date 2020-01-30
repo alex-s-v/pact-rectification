f2a27cd8eca47532d188aeddfd7d9fefebd6b56a
Alexandr Vasilyev <alexandr.s.vasilyev@gmail.com>
2020-01-30 16:35:45 +0300

    Add ping dependency

--- setup.py
+++ setup.py
@@ -18,75 +18,76 @@ from setuptools import setup
 def read(*names, **kwargs):
     with io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='rectification',
-    version='0.1.0',
-    license='MIT',
-    description='Simple rectification simulation software.',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="rectification",
+    version="0.1.0",
+    license="MIT",
+    description="Simple rectification simulation software.",
+    long_description="%s\n%s" % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub("", read("README.rst")),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))
     ),
-    author='Alexandr Vasilyev',
-    author_email='alexandr.s.vasilyev@gmail.com',
-    url='https://github.com/alex-s-v/pact-rectification',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Alexandr Vasilyev",
+    author_email="alexandr.s.vasilyev@gmail.com",
+    url="https://github.com/alex-s-v/pact-rectification",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        # "Programming Language :: Python :: 2.7",
+        # "Programming Language :: Python :: 3",
+        # "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
-        'Private :: Do Not Upload',
+        # "Programming Language :: Python :: Implementation :: IronPython",
+        # "Programming Language :: Python :: Implementation :: Jython",
+        # "Programming Language :: Python :: Implementation :: Stackless",
+        "Topic :: Utilities",
+        "Private :: Do Not Upload",
     ],
     project_urls={
-        'Documentation': 'https://pact-rectification.readthedocs.io/',
-        'Changelog': 'https://pact-rectification.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/alex-s-v/pact-rectification/issues',
+        "Documentation": "https://pact-rectification.readthedocs.io/",
+        "Changelog": "https://pact-rectification.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/alex-s-v/pact-rectification/issues",
     },
     keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
+        # eg: "keyword1", "keyword2", "keyword3",
     ],
-    python_requires='>=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires=">=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        "pint"
+        # eg: "aspectlib==1.1.1", "six>=1.7",
     ],
     extras_require={
         # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
+        #   "rst": ["docutils>=0.11"],
+        #   ":python_version=="2.6"": ["argparse"],
     },
     entry_points={
-        'console_scripts': [
-            'rectification = rectification.cli:main',
+        "console_scripts": [
+            "rectification = rectification.cli:main",
         ]
     },
-    dependency_links = [
+    dependency_links=[
         "https://bitbucket.org/alex-s-v/thermo_extra/src/master#egg=thermo_extra"
     ],
 )
