6a3bbf46333e48cc5d0778c5aec5978a8bf12986
Alexandr Vasilyev <alexandr.s.vasilyev@gmail.com>
2020-02-13 16:47:14 +0300

    SI units for common properties and warning dumping

--- src/rectification/core.py
+++ src/rectification/core.py
@@ -1,6 +1,25 @@
 """
 Module with core fuctionality of this package.
 """
+import warnings
 import pint
 
+
 ureg = pint.UnitRegistry()
+
+# Disable new NumPy - pint interaction warning
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    ureg.Quantity([])
+
+
+propSI = {
+    "molar mass": ureg.g / ureg.mol,
+    "viscosity": ureg.Pa * ureg.s,
+    "surface tension": ureg.N / ureg.m,
+    "molar volume": ureg.m**3 / ureg.mol,
+    "density": ureg.kg / ureg.m**3,
+    "mass enthalpy": ureg.J / ureg.kg,
+    "thermal conductivity": ureg.W / ureg.m / ureg.K,
+    "heat capacity": ureg.J / ureg.kg / ureg.K
+}
