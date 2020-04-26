036266610f152e5e90032777e21e769712fdf73b
Daniel <parrot123@mail.ru>
2020-04-26 17:18:03 +0300

    Test

--- src/rectification/equations.py
+++ src/rectification/equations.py
@@ -4,9 +4,9 @@ import numpy as np
 Vm = 22.4
 
 
+#def operating_line(x, R, xp):
+    #return R * x / (R + 1) + xp / (R + 1)
 def operating_line(x, R, xp):
-    return R * x / (R + 1) + xp / (R + 1)
-
 
 @unitcheck(L="kg/s", rhol="kg/m**3", Ft="m**2", res_unit="m**3/(m**2*s)")
 def U_coef(L, rhol, Ft):
