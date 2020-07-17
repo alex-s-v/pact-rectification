3321a958faaa45f1185f628d0c918e8c51383f16
Daniel <parrot123@mail.ru>
2020-07-17 17:48:34 +0300

    Add Waste cooler

--- src/rectification/equations/Cooler_waste.py
+++ src/rectification/equations/Cooler_waste.py
@@ -27,269 +27,273 @@ def deltaT_diff_waste(deltaT_larger_waste, deltaT_less_waste):
         return (deltaT_larger_waste - deltaT_less_waste) / np.log(deltaT_larger_waste / deltaT_less_waste)
 
 
-def deltaT_larger(t_vapor, tinit_mix):
+def deltaT_larger_waste(tw, t_coolwater_exit):
     """
-    Calculates the difference temperatures between the temperature of vapor and the lowest temperature of liquid.
+    Calculates the difference temperatures between the temperature of boiling waste and the exit temperature of coolwater.
     Parameters
     ----------
-    t_vapor : float
-        The temperature of vapor, [degrees celcium]
-    tinit_mix : float
-        The initial temperature of mix, [degrees celcium]
+    tw : float
+        The temperature of boiling waste, [degrees celcium]
+    t_coolwater_exit : float
+        The exit temperature of coolwater, [degrees celcium]
     Returns
     -------
-    deltaT_larger : float
-        The difference temperatures between the temperature of vapor and the lowest temperature of liquid , [degrees celcium]
+    deltaT_larger_waste : float
+        The difference temperatures between the temperature of boiling waste and the exit temperature of coolwater, [degrees celcium]
     References
     ----------
     Романков, формула 4.78, стр.169
     """       
-    return t_vapor - tinit_mix
+    return tw - t_coolwater_exit
 
 
-def deltaT_less(t_vapor, tboil_mix):
+def deltaT_less_waste(tw, t_coolwater_enter):
     """
-    Calculates the difference temperatures between the temperature of vapor and the lowest temperature of liquid.
+    Calculates the difference temperatures between the temperature of boiling waste and the enter temperature of coolwater.
     Parameters
     ----------
-    t_vapor : float
-        The temperature of vapor, [degrees celcium]
-    tboil_mix : float
-        The boilling temperature of mix, [degrees celcium]
+    tw : float
+        The temperature of boiling waste, [degrees celcium]
+    t_coolwater_enter : float
+        The enter temperature of coolwater, [degrees celcium]
     Returns
     -------
-    deltaT_less : float
-        The difference temperatures between the temperature of vapor and the biggest temperature of liquid, [degrees celcium]
+    deltaT_less_waste : float
+        the difference temperatures between the temperature of boiling waste and the enter temperature of coolwater, [degrees celcium]
     References
     ----------
     Романков, формула 4.78, стр.169
     """       
-    return t_vapor - tboil_mix
+    return tw - t_coolwater_enter
 
 
-@unitcheck(deltaT="degrees celcium", F_mass="kg/s", Cp="J/(kg * degrees celcium", r_feed="J/kg", res_unit="W")
-def Q_feed(deltaT_waste, W_mass, Cw, t1_cooler_waste, ):
+@unitcheck(deltaT_waste="degrees celcium", F_mass="kg/s", Cp="J/(kg * degrees celcium", r_feed="J/kg", res_unit="W")
+def Q_wastecooler(W_mass, Cw, t_coolwater_exit, tw):
     """
-    Calculates the heat load of heat exchanger.
+    Calculates the heat load of waste cooler.
     Parameters
     ----------
-    deltaT : float
-        The difference temperatures between the initial and the ultimate temperatures of mix , [degrees celcium]
-    F_mass : float
-        The mass flow rate of feed [kg/s]
-    Cp : float
-        The heat capacity of mix [J/(kg * degrees C)]
-    phi_vapor: float
-        The part of vapor in feed, [dimensionless]
-    r_feed : float
-        The heat vaporazation of mix, [J/kg]
+    W_mass : float
+        The mass flow rate of waste, [kg/s]
+    Cw : float
+        The heat capacity of waste, [J/(kg * degrees C)]
+    t_coolwater_exit: float
+        The end temperature of cool waste, [degrees C]
+    tw : float
+        The temperature of boiling waste, [degrees celcium]
     Returns
     -------
-    Q_feed : float
-        The heat load of heat exchanger, [W] , [J/s]
+    Q_wastecooler : float
+        The heat load of waste cooler, [W] , [J/s]
     References
     ----------
     Дытнерский, формула 2.2, стр.45
     """   
-    return F_mass * (Cp * deltaT + phi_vapor * r_feed)
+    return W_mass * Cw * (tw - t_coolwater_exit)
 
 
-def deltaT(tinit_mix, tboil_mix):
-    """
-    Calculates the difference temperatures between the initial and the ultimate temperatures of mix , [degrees celcium]
-    Parameters
-    ----------
-    tinit_mix : float
-        The initial temperature of mix, [degrees celcium]
-    tboil_mix : float
-        The boilling temperature of mix, [degrees celcium]
-    Returns
-    -------
-    deltaT : float
-        The difference temperatures between the initial and the ultimate temperatures of mix , [degrees celcium]
-    References
-    ----------
-    Дытнерский, формула 2.2, стр.45
-    """       
-    return tinit_mix - tboil_mix
-
-
-
-@unitcheck(Q_feed="W", deltaT_diff="degrees celcium",  Kt_approx="W/(m**2 * degrees celcium", res_unit="m**2")
-def A_approx(Q_feed, deltaT_diff, Kt_approx):
+@unitcheck(Q_wastecooler="W", deltaT_diff_waste="degrees celcium",  Kt_approx_waste="W/(m**2 * degrees celcium", res_unit="m**2")
+def A_approx_waste(Q_wastecooler, deltaT_diff_waste, Kt_approx_waste):
     """
     Calculates the approximate heatransfer area.
     Parameters
     ----------
-    Q_feed : float
-        The heat load of heat exchanger, [W] , [J/s]
-    deltaT_diff : float
+    Q_wastecooler : float
+        The heat load of waste cooler, [W] , [J/s]
+    deltaT_diff_waste : float
         The coefficient difference of temperatures, [degrees celcium]
-    Kt_approx : float
+    Kt_approx_waste : float
         The heatransfer coefficient [W/(m**2 * degrees celcium)]
     Returns
     -------
-    A_approx : float
+    A_approx_waste : float
         The approximate heatransfer area, [m**2]
     References
     ----------
     Романков, формула 4.72, стр.168
     """           
-    return Q_feed / (deltaT_diff * Kt_approx)
+    return Q_wastecooler / (deltaT_diff_waste * Kt_approx_waste)
 
 
-@unitcheck(Q_feed="J/s", r_steam="J/kg", res_unit="kg/s")
-def m_coolwater_waste(Q_waste_cooler, r_steam):
+@unitcheck(Q_wastecooler="J/s", r_steam="J/kg", res_unit="kg/s")
+def m_coolwater_waste(Q_wastecooler, Cp_wastecooler, t_coolwater_exit, t_coolwater_enter):
     """
-    Calculates the flow rate steam of boiler.
+    Calculates the flow rate steam of waste cooler.
     Parameters
     ----------
-    Q_feed : float
-        The heat load feed of heat exchanger, [W] [J/s]
-    r_steam : float
-        The heat vaporazation of dist [J/kg]
+    Q_wastecooler : float
+        The heat load feed of waste cooler, [W] [J/s]
+    Cp_wastecooler : float
+        The heat capacity of water, [J/(kg * degrees C)]
+    t_coolwater_exit: float
+        The end temperature of cool waste, [degrees C]
+    t_coolwater_enter: float
+        The start temperature of cool waste, [degrees C]    
     Returns
     -------
-    m_vapor_feed : float
-        The flow rate steam of feed heat exchanger, [kg/s]
+    m_coolwater_waste : float
+        The flow rate steam of waste cooler, [kg/s]
     References
     ----------
     Дытнерский, формула 2.3, стр.45
     """               
-    return Q_feed / r_steam
+    return Q_wastecooler / (Cp_wastecooler * (t_coolwater_exit - t_coolwater_enter))
     
 
-@unitcheck(F_mass="kg/s", d_inner="m", mu_feed="Pa/s")
-def Re(F_mass, z_way, d_inner, n_pipe, mu_feed):
+@unitcheck(W_mass="kg/s", d_inner_waste="m", mu_waste_avrg="Pa/s")
+def Re_intube_waste(W_mass, z_way_waste, d_inner_waste, n_pipe_waste, mu_waste_avrg):
     """
     Calculates the Reynold criterion.
     Parameters
     ----------
     F_mass : float
         The mass flow rate of feed [kg/s]
-    z_way : float
+    z_way_waste : float
         The number of ways in heat exchanger [dimensionless]
-    d_inner : float
+    d_inner_waste : float
         The diametr of inner pipe, [m]
-    n_pipe : float
+    n_pipe_waste : float
         The number of pipes in heat exchanger, [dimensionless]
-    mu_feed : float
+    mu_waste_avrg : float
         The mix viscocity of liquid, [Pa/s]
     Returns
     -------
-    Re : float
-        The Reynold criterion, [dimensionless]
+    Re_intube_waste : float
+        The Reynold criterion in tube(waste), [dimensionless]
     References
     ----------
     &&&&&&
     """         
-    return 0.785 * F_mass * z_way / (d_inner * n_pipe * mu_feed)
+    return 0.785 * W_mass * z_way_waste / (d_inner_waste * n_pipe_waste * mu_waste_avrg)
 
 
-@unitcheck(C_feed="J/(kg * degrees celcium)", mu_feed="Pa/s", lyambda_feed="W / (m * degreec celcium)")
-def Pr(C_feed, mu_feed, lyambda_feed):
+@unitcheck(m_coolwater_waste="kg/s", d_outer_waste="m", mu_feed="Pa/s")
+def Re_outtube_waste(m_coolwater_waste, d_outer_waste, mu_coolwater_waste):
     """
-    Calculates the Prandtl criterion.
+    Calculates the Reynold criterion.
     Parameters
     ----------
-    C_feed : float
-        The heat capacity of mix [J/(kg * degrees celcium)]
-    mu_feed : float
-        The mix viscocity of liquid, [Pa/s]
-    lyambda_feed : float
-        The thermal conductivity of feed, [W / (m * degreec celcium)]
+    m_coolwater_waste : float
+        The mass flow rate of feed [kg/s]
+    d_outer_waste : float
+        The diametr of outer pipe, [m]
+    mu_coolwater_waste : float
+        The mix viscocity of coolwater, [Pa/s]
     Returns
     -------
-    Pr : float
-        The Prandtl criterion, [dimensionless]
+    Re_outtube_waste : float
+        The Reynold criterion in outer tube(waste), [dimensionless]
+    References
+    ----------
+    &&&&&&
+    """         
+    return 0.785 * m_coolwater_waste * d_outer_waste / ( * mu_coolwater_waste)
+
+
+@unitcheck(Cp_wastecooler="J/(kg * degrees celcium)", mu_coolwater_waste="Pa/s", lyambda_coolwater_waste="W / (m * degreec celcium)")
+def Pr_coolwater_waste(Cp_wastecooler, mu_coolwater_waste, lyambda_coolwater_waste):
+    """
+    Calculates the Prandtl criterion of coolwater.
+    Parameters
+    ----------
+    Cp_wastecooler : float
+        The heat capacity of water, [J/(kg * degrees C)]
+    mu_coolwater_waste : float
+        The mix viscocity of coolwater, [Pa/s]
+    lyambda_coolwater_waste : float
+        The thermal conductivity of coolwater, [W / (m * degreec celcium)]
+    Returns
+    -------
+    Pr_coolwater_waste : float
+        The Prandtl criterion of coolwater, [dimensionless]
     References
     ----------
     Романков, формула 4.12, стр.151
     """        
-    return C_feed * mu_feed / lyambda_feed
+    return Cp_wastecooler * mu_coolwater_waste / lyambda_coolwater_waste
 
 
-def Nu(Re, Pr):
+def Nu_waste(Re_outtube_waste, Pr_coolwater_waste):
     """
-    Calculates the Nusselt criterion.
+    Calculates the Nusselt criterion of coolwater.
     Parameters
     ----------
-    Re : float
+    Re_outtube_waste : float
         The Reynold criterion, [dimensionless]
-    Pr : float
+    Pr_coolwater_waste : float
         The Prandtl criterion, [dimensionless]
     Returns
     -------
-    Nu : float
+    Nu_waste : float
         The Nusselt criterion, [dimensionless]
     References
     ----------
     Романков, формула 4.17, стр.152
     """      
-    return 0.021 * (Re**0.8) * (Pr**0.4)
+    return 0.24 * (Re_outtube_waste**0.6) * (Pr_coolwater_waste**0.36)*0,93
 
 
-@unitcheck(lyambda_feed="W / (m * degreec celcium)", d_inner="m", res_unit="W / (m**2 * degrees celcium)")
-def alpha_liq(Nu, lyambda_feed, d_inner):
+@unitcheck(lyambda_coolwater_waste="W / (m * degreec celcium)", d_outer_waste="m", res_unit="W / (m**2 * degrees celcium)")
+def alpha_coolwater_waste(Nu_waste, lyambda_coolwater_waste, d_outer_waste):
     """
     Calculates the coefficent of heat transfer(alpha) from liquid to wall of pipe.
     Parameters
     ----------
-    Nu : float
+    Nu_waste : float
         The Nusselt criterion, [dimensionless]
-    lyambda_feed : float
-        The thermal conductivity of feed, [W / (m * degreec celcium)]
-    d_inner : float
-        The diametr of inner pipe, [m]
+    lyambda_coolwater_waste : float
+        The thermal conductivity of coolwater, [W / (m * degreec celcium)]
+    d_outer : float
+        The diametr of outer pipe, [m]
     Returns
     -------
-    alpha_liq : float
+    alpha_coolwater_waste : float
         The coefficent of heat transfer(alpha), [W / (m**2 * degrees celcium)]
     References
     ----------
     Романков, формула 4.11, стр.150
     """          
-    return Nu * lyambda_feed / d_inner
+    return Nu_waste * lyambda_coolwater_waste / d_outer_waste
 
 
-@unitcheck(lyambda_cond="W / (m * degrees celcium)", rho_cond="kg / m**3",  mu_cond="Pa / s", m_vapor_feed="kg/s", d_outside="m", res_unit="W / (m**2 * degrees celcium)")
-def alpha_vap(lyambda_cond, rho_cond, mu_cond, m_vapor_feed, n_pipe, d_outside):
+@unitcheck(lyambda_cond_waste_avrg="W / (m * degrees celcium)", rho_cond_waste_avrg="kg / m**3",  mu_cond_waste_avrg="Pa / s", W_mass="kg/s", L_waste="m", res_unit="W / (m**2 * degrees celcium)")
+def alpha_waste(lyambda_cond_waste_avrg, rho_cond_waste_avrg, mu_cond_waste_avrg, W_mass, n_pipe_waste, L_waste):
     """
     Calculates the coefficent of heat transfer(alpha) from steam to wall of pipe.
     Parameters
     ----------
-    lyambda_cond : float
+    lyambda_cond_waste_avrg : float
         The thermal conducivity of condensate, [W / (m * degrees celcium)]
-    rho_cond : float
+    rho_cond_waste_avrg : float
         The destiny of condensate, [kg / m**3]
-    mu_cond : float
+    mu_cond_waste_avrg : float
         The viscosity of condensate, [Pa / s]
-    m_vapor_feed : float
+    W_mass : float
         The flow rate steam of feed heat exchanger, [kg/s]
-    n_pipe : float
+    n_pipe_waste : float
         The number of pipes in heat exchanger, [dimensionless]
-    d_outside : float
-        The outside diameter of pipe, [m]    
+    L_waste : float
+        The length of tubes, [m]    
     Returns
     -------
-    alpha_vap : float
+    alpha_waste : float
         The coefficent of heat transfer(alpha) from steam to wall of pipe, [W / (m**2 * degrees celcium)]
     References
     ----------
     Дытнерский, формула 2.24, стр.53
     """              
-    return lyambda_cond * 3.78 * ((rho_cond**2)* n_pipe * d_outside / (mu_cond * m_vapor_feed))**(1/3)
+    return lyambda_cond_waste_avrg * 2.02 * ((rho_cond_waste_avrg**2)* L_waste * n_pipe_waste / (mu_cond_waste_avrg * W_mass))**(1/3)
 
 
-@unitcheck(pollution_1="m**2 * degrees celcium / W", pollution_2="m**2 * degrees celcium / W", sigma="m",  lyambda_wall="W / (m * degrees celcium)", res_unit="m**2 * degrees celcium / W")
-def sigma_thermpollution(pollution_1, pollution_2, sigma, lyambda_wall):
+@unitcheck(pollution_1_waste="m**2 * degrees celcium / W", pollution_2_waste="m**2 * degrees celcium / W", sigma="m",  lyambda_wall="W / (m * degrees celcium)", res_unit="m**2 * degrees celcium / W")
+def sigma_thermpollution_waste(pollution_1_waste, pollution_2_waste, sigma, lyambda_wall):
     """
     Calculates the sum of thermal pollutions.
     Parameters
     ----------
-    pollution_1 : float
+    pollution_1_waste : float
         The thermal pollution of the first coolant, [m**2 * degrees celcium / W]
-    pollution_2 : float
+    pollution_2_waste : float
         The thermal pollution of the second coolant, [m**2 * degrees celcium / W]
     sigma : float
         The thickness of pipe wall, [m]
@@ -297,69 +301,69 @@ def sigma_thermpollution(pollution_1, pollution_2, sigma, lyambda_wall):
         The thermal conducivity of wall, [W / (m * degrees celcium)]  
     Returns
     -------
-    sigma_thermpollution : float
+    sigma_thermpollution_waste : float
         The the sum of thermal pollutions, [m**2 * degrees celcium / W]
     References
     ----------
     &&&&&
     """                  
-    return (sigma / lyambda_wall) + (1 / pollution_1) + (1 / pollution_2)
+    return (sigma / lyambda_wall) + (1 / pollution_1_waste) + (1 / pollution_2_waste)
 
 
-@unitcheck(alpha_liq="W / (m**2 * degrees celcium)", alpha_vap="W / (m**2 * degrees celcium)",  sigma_thermpollution="W/(m**2 * degrees celcium", res_unit="W/(m**2 * degrees celcium")
-def Kt_real(alpha_liq, alpha_vap, sigma_thermpollution):
+@unitcheck(alpha_waste="W / (m**2 * degrees celcium)", alpha_coolwater_waste="W / (m**2 * degrees celcium)",  sigma_thermpollution_waste="W/(m**2 * degrees celcium", res_unit="W/(m**2 * degrees celcium")
+def Kt_real_waste(alpha_waste, alpha_coolwater_waste, sigma_thermpollution_waste):
     """
     Calculates the coefficient of heat transfer (Kt).
     Parameters
     ----------
-    alpha_liq : float
+    alpha_waste : float
         The coefficent of heat transfer(alpha), [W / (m**2 * degrees celcium)]
-    alpha_vap : float
+    alpha_coolwater_waste : float
         The coefficent of heat transfer(alpha) from steam to wall of pipe, [W / (m**2 * degrees celcium)]
-    sigma_thermpollution : float
+    sigma_thermpollution_waste : float
         The the sum of thermal pollutions, [m**2 * degrees celcium / W]  
     Returns
     -------
-    Kt_real : float
+    Kt_real_waste : float
         The coefficient of heat transfer (Kt), [W / (m**2 * degrees celcium)]
     References
     ----------
     Романков, формула 4.74, стр. 168
     """      
-    return ((1 / alpha_liq) + (1 / alpha_vap) + (sigma_thermpollution))**-1
+    return ((1 / alpha_waste) + (1 / alpha_coolwater_waste) + (sigma_thermpollution_waste))**-1
 
 
-@unitcheck(Q_feed="W", deltaT_diff="degrees celcium",  Kt_approx="W/(m**2 * degrees celcium", res_unit="m**2")
-def A_real(Q_feed, Kt_real, deltaT_diff):
+@unitcheck(Q_wastecooler="W", deltaT_diff_waste="degrees celcium",  Kt_real_waste="W/(m**2 * degrees celcium", res_unit="m**2")
+def A_real_waste(Q_wastecooler, Kt_real_waste, deltaT_diff_waste):
     """
     Calculates the real heatransfer area.
     Parameters
     ----------
-    Ql_feed : float
-        The heat load of heat exchanger, [W] , [J/s]
-    deltaT_diff : float
+    Q_wastecooler : float
+        The heat load of wastecooler, [W] , [J/s]
+    deltaT_diff_waste : float
         The coefficient difference of temperatures, [degrees celcium]
-    Kt_real : float
-        The heat ransfer coefficient [W/(m**2 * degrees celcium)]
+    Kt_real_waste : float
+        The heat transfer coefficient [W/(m**2 * degrees celcium)]
     Returns
     -------
-    A_real : float
+    A_real_waste : float
         The real heat ransfer area, [m**2]
     References
     ----------
     Романков, формула 4.72, стр.168
     """      
-    return Q_wastecooler / (Kt_wastecooler_real * deltaT_diff)
+    return Q_wastecooler / (Kt_real_waste * deltaT_diff_waste)
 
 
-def surface_margin_wastecooler (A_approx_wastecooler, A_real_wastecooler):
+def surface_margin_waste (A_approx_waste, A_real_waste):
     """
     Calculates the surface margin.
     Parameters
     ----------
-    A_approximate_wastecooler : float
+    A_approximate_waste : float
         The approximate heat ransfer area, [m**2]
-    A_real_wastecooler : float
+    A_real_waste : float
         The real heat transfer area, [m**2]
     Returns
     -------
@@ -369,4 +373,4 @@ def surface_margin_wastecooler (A_approx_wastecooler, A_real_wastecooler):
     ----------
     &&&&
     """          
-    return (A_approx_wastecooler - A_real_wastecooler) * 100 / A_approx
+    return (A_approx_waste - A_real_waste) * 100 / A_approx_waste
